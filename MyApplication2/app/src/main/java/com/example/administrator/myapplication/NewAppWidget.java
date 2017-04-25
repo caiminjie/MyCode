5ba268924b09432fa1977a685e3b2056237f38bb
unknown <>
2017-04-26 02:46:11 +0800

    uodate a demo apk

---
+++ MyApplication2/app/src/main/java/com/example/administrator/myapplication/NewAppWidget.java
@@ -0,0 +1,52 @@
+package com.example.administrator.myapplication;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NewAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) throws Exception{
+        for(int i =0;i <=10000;i++ ) {
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+            String a = "edward" + i;
+        // Construct the RemoteViews object
+            Log.e("edward","the number is : "+ i);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        views.setTextViewText(R.id.appwidget_text, a);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.partiallyUpdateAppWidget(appWidgetId, views);
+            Thread.currentThread().sleep(5000);
+        }
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            try {
+                updateAppWidget(context, appWidgetManager, appWidgetId);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
